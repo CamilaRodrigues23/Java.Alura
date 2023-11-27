f5acffb1e3bd339a9a29fa2bb4d9ff97e0f4a0c8
Camila Rodrigues <114814915+CamilaRodrigues23@users.noreply.github.com>
2023-11-27 17:27:42 -0300

    Add files via upload
    Explorando a linguagem Java Script com as aulas da Alura cursos.

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,10 @@
+console.log("Meu primeiro Programa:Trabalhando com Variáveis");
+
+const idade = 26;
+
+console.log(idade);
+console.log(idade+2);
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
+
